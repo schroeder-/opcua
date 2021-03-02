0a9770860b76e31dc625a6a4e6bb035cadaa00e3
Adam Lock <locka99@gmail.com>
2021-03-02 23:59:18 +0000

    More messing around with Glade & GTK

--- tools/explorer/src/main.rs
+++ tools/explorer/src/main.rs
@@ -1,48 +1,103 @@
-use gtk::{self, prelude::*};
+use std::rc::Rc;
 
-mod new_connection_dlg;
+use glib::clone;
+use gtk::{self, prelude::*};
 
 use new_connection_dlg::NewConnectionDlg;
 
+mod new_connection_dlg;
+
 fn main() {
-    if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
-        return;
+    App::run();
+}
+
+struct App {
+    pub builder: Rc<gtk::Builder>,
+    pub model: AppModel,
+    toolbar_connect_btn: Rc<gtk::ToolButton>,
+    toolbar_disconnect_btn: Rc<gtk::ToolButton>,
+}
+
+struct AppModel {}
+
+impl App {
+    pub fn run() {
+        if gtk::init().is_err() {
+            println!("Failed to initialize GTK.");
+            return;
+        }
+
+        // The user interface is defined as a .glade file
+        let glade_src = include_str!("ui.glade");
+        let builder = Rc::new(gtk::Builder::from_string(glade_src));
+
+        let toolbar_connect_btn: Rc<gtk::ToolButton> =
+            Rc::new(builder.get_object("toolbar_connect_btn").unwrap());
+
+        let toolbar_disconnect_btn: Rc<gtk::ToolButton> =
+            Rc::new(builder.get_object("toolbar_disconnect_btn").unwrap());
+
+        let app = Rc::new(App {
+            builder,
+            toolbar_connect_btn,
+            toolbar_disconnect_btn,
+            model: AppModel {},
+        });
+
+        // Hook up the toolbar buttons
+        app.toolbar_connect_btn
+            .connect_clicked(clone!(@weak app => move |_| {
+                app.on_connect();
+            }));
+
+        app.toolbar_disconnect_btn
+            .connect_clicked(clone!(@weak app => move |_| {
+                app.on_disconnect();
+            }));
+
+        // Address space explorer pane
+        // TODO
+
+        // Monitored item pane
+        // TODO
+
+        // Monitored item properties pane
+        // TODO
+
+        // Log / console window
+        // TODO
+
+        let main_window: gtk::ApplicationWindow = app.builder.get_object("main_window").unwrap();
+        main_window.connect_delete_event(|_, _| {
+            println!("Application is closing");
+            gtk::main_quit();
+            Inhibit(false)
+        });
+
+        app.update_state();
+
+        main_window.show_all();
+
+        gtk::main();
     }
 
-    // The user interface is defined as a .glade file
-    let glade_src = include_str!("ui.glade");
-    let builder = gtk::Builder::from_string(glade_src);
-
-    let main_window: gtk::ApplicationWindow = builder.get_object("main_window").unwrap();
-
-    // Address space explorer pane
-    // TODO
-
-    // Monitored item pane
-    // TODO
-
-    // Monitored item properties pane
-    // TODO
-
-    // Log / console window
-    // TODO
-
-    // TODO this button will be removed in due course
-    let btn: gtk::Button = builder.get_object("test").unwrap();
-    btn.connect_clicked(move |_| {
+    pub fn on_connect(&self) {
         println!("Clicked!");
-        let dlg = NewConnectionDlg::new(&builder);
+        let dlg = NewConnectionDlg::new(&self.builder);
         dlg.show();
-    });
+    }
 
-    main_window.connect_delete_event(|_, _| {
-        println!("Application is closing");
-        gtk::main_quit();
-        Inhibit(false)
-    });
+    pub fn on_disconnect(&self) {
+        println!("Disconnect Clicked!");
+    }
 
-    main_window.show_all();
+    fn is_connected(&self) -> bool {
+        false
+    }
 
-    gtk::main();
+    pub fn update_state(&self) {
+        let is_connected = self.is_connected();
+        self.toolbar_connect_btn.set_sensitive(!is_connected);
+        self.toolbar_disconnect_btn.set_sensitive(is_connected);
+    }
 }
